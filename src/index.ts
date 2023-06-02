77c5068d24e832ba993ebbaa61182f017d642924
zhaoqiaochu <c9cu@qq.com>
2023-06-02 13:27:13 +0800

    test input and output

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import core from '@actions/core'
+import github from '@actions/github'
+
+try {
+  const repository = core.getInput('repository')
+  console.log('repository: ', repository)
+  const [namespace, repositoryName] = repository.split('/')
+  const requestUrl = `https://hub.docker.com/v2/namespaces/${namespace}/repositories/${repositoryName}/tags`
+  console.log('requestUrl: ', requestUrl)
+  fetch(requestUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      core.setOutput('data', data)
+    })
+} catch (error: any) {
+  core.setFailed(error.message)
+}
